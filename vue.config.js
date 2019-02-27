1555019d83d3403187feb8d2f9a7be2d2c1ec77e
tigerwill90 <sylvain.muller90@gmail.com>
2019-02-27 22:42:13 +0100

    remove prettierrc

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   devServer: {
     port: 9000
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/]
-};
+}
